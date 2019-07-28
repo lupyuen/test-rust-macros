6a609f1ea3d3787c4573c5228d2c2c79c497aeeb
lupyuen <luppy@appkaki.com>
2019-07-28 11:20:42 +0800

    Fixed structure

--- src/main.rs
+++ src/main.rs
@@ -2,256 +2,24 @@
 #![feature(proc_macro_hygiene)]  //  Allow proc macros to be unhygienic
 #![feature(custom_attribute)]    //  Allow custom attributes like [safe_wrap]
 
-/// Given an expression `e`, add 88. If identifier `i` and statement block `blk` are specified, add `i` to the result and execute `blk`.
-macro_rules! add_88 {
-    // If `e` is an expression like `123`, `i * 2`, `func(i)`...
-    ($e:expr) => {
-        {
-            // Add 88 to the expression and return it.
-            $e + 88
-        }
-    };
-    // If `e` is an expression, `i` is an identifier like `myvar`, `blk` is a block of statements...
-    ($e:expr, $i:ident, $blk:block) => {
-        {
-            // Add 88 and the value of `i` to the expression.
-            let result = $e + $i + 88;
-            // Execute the code block.
-            $blk;
-            // Return the result.
-            result
-        }
-    }
-}
-
-///  Macro to dump all tokens received as a literal string, e.g.
-///  `d!(a b c)` returns `"a b c"`
-macro_rules! d {
-  //  This rule matches zero or more tokens.
-  ($($token:tt)*) => {
-    //  For all matched tokens, convert into a string.
-    stringify!($($token)*)
-  };
-}
-
-macro_rules! parse {
-  // Helper macro to parse a JSON `key: value` entry. The entry should be followed by a trailing comma.
-  // For example: When parsing the JSON code `{ "device": "010203" , (omitted) }`, the macro will be called like this:
-  // ```
-  // parse!( @json @object context ["device"] ("010203") , (omitted) )
-  // ```
-  (@$enc:ident @object $obj:ident [$($key:tt)+] ($value:expr) , $($rest:tt)*) => {
-    d!(adding key: $($key)+ value: $value to object: $obj)
-
-    //  Append to the "values" array e.g.
-    //    {"key":"device", "value":"010203"},
-    //  $crate::coap_item_str!(@$enc $obj, $($key)+, $value);
-
-    //  Continue expanding the rest of the JSON.
-    //  $crate::parse!(@$enc @object $obj () ($($rest)*) ($($rest)*));
-  };
-}
-
-/// Let's make some soup based on a recipe...
-macro_rules! bad_soup {
-    // The caller shall pass in a recipe for the soup, 
-    // say `salt + 88`
-    ($recipe:expr) => {
-        {
-            // We use our own salt, say Table Salt.
-            let salt = 1;
-            // We try to use our salt, but the recipe 
-            // actually requires a different salt, 
-            // like Sea Salt.
-            $recipe
-            // Rust Compiler fails with a Hygiene Error.
-        }
-    }
-}
-
-/// Let's make soup the right way...
-macro_rules! good_soup {
-    // The caller shall pass in 2 things: 
-    // Which salt to use
-    // and the recipe (like `salt + 88`)
-    ($salt:ident, $recipe:expr) => {
-        {
-            // We use whatever salt the caller passes in. 
-            // Hence we use `$salt` instead of `salt`.
-            let $salt = 1;
-            // Then we use the same salt inside the recipe.
-            $recipe
-            // It works!
-        }
-    }
-}
-
-/// Make an Unhygienic soup... 
-fn make_bad_soup() -> Result<i32, ()> {
-    let soup = bad_soup!(
-        // We try to make soup with our salt and recipe...
-        88 + salt  // If you're testing other macros, comment out the `+ salt` part or the code will never compile
-        // But the salt isn't the same inside the recipe.  
-        // Rust Compiler fails with Hygiene Error:
-        // `salt` not found in this scope.
-    );
-    Ok(soup)  //  Return the cooked soup.
-}
-
-/// Make a Hygienic soup
-fn make_good_soup() -> Result<i32, ()> {
-    let soup = good_soup!(
-        // First we tell the macro which salt we're using...
-        salt, 
-        // Then we tell the macro what to do with that salt.
-        88 + salt
-        //  It works!
-    );
-    Ok(soup)  //  Return the cooked soup.
-}
-
-/// Let's test cooking!
-#[cfg(test)]
-mod tests {
-    use super::*;  //  Import everything from outer scope.
-
-    /// Test the simple add_88 macro with 1 parameter.
-    #[test]
-    fn test_add_88() { 
-        assert_eq!(
-            89,
-            add_88!(1),
-        );
-    }
-
-    /// Test the extended add_88 macro with 3 parameters.
-    #[test]
-    fn test_add_88_extended() { 
-        let x = 2;
-        let y = 3;
-        assert_eq!(
-            99,
-            add_88!(
-                //  Expression
-                x * 4,
-                //  Identifier
-                y,
-                //  Code Block
-                { println!("The values of x and y are {} and {}", x, y); }
-            )
-        );
-        // Shows `The value of x and y are 2 and 3`. Result is 99.
-    }
-
-    #[test]
-    fn test_parse() { 
-        parse! (
-            @json @object context ["device"] ("010203") , omitted 
-        );
-    }
-
-    #[test]
-    fn test_bad_soup() { 
-        assert_eq!(
-            make_bad_soup(),
-            Ok(89)
-        );
-    }
-
-    #[test]
-    fn test_good_soup() { 
-        assert_eq!(
-            make_good_soup(),
-            Ok(89)
-        );
-    }
-}
-
-/// Let's start cooking!
-fn main() {
-    trace_macros!(true);   //  Start tracing macros
-    let res = add_88!(1);
-    trace_macros!(false);  //  Stop tracing macros
-    println!("test_add_88: {}", res);
-
-    parse! (
-        @json @object context ["device"] ("010203") , (omitted) 
-    );
-    println!("bad soup: {}",
-        make_bad_soup().unwrap()
-    );
-    println!("good soup: {}",
-        make_good_soup().unwrap()
-    );
-}
-
 /// Testing safe_wrap
-////#[cfg(test_safe_wrap_macro)]
-mod tests {
-    extern crate mynewt_macros;
-    use mynewt_macros::{out, strn, init_strn}; //  Import Mynewt macros from `macros` library
+#[cfg(feature = "test_safe_wrap")]
+mod test_safe_wrap {
+    extern crate macros;
+    use macros::{init_strn}; //  Import Mynewt macros from `macros` library
+
+    extern crate mynewt;
+    use mynewt::{
+        result::*,            //  Import Mynewt result and error types
+        kernel::os::os_eventq,
+    };
 
     ///////////////////////////////////////////////////////////////////////////////
     //  Testing
 
-    /// Represents a null-terminated byte string, suitable for passing to Mynewt APIs as `* const char`
-    pub struct Strn {
-        /// Byte string terminated with null
-        pub bytestr: &'static [u8]
-    }
-
-    impl Strn {
-        /// Create a new byte string. Fail if the last byte is not zero.
-        /// ```
-        /// Strn::new(b"network\0")
-        /// strn!("network")
-        /// ```
-        pub fn new(bs: &'static [u8]) -> Strn {
-            //  Last byte must be 0.
-            assert_eq!(bs.last(), Some(&0u8));
-            let res = Strn { bytestr: bs };
-            res
-        }
-
-        /// Return the byte string as a null-terminated `* const char` C-style string.
-        /// Fail if the last byte is not zero.
-        pub fn as_cstr(self) -> *const ::cty::c_char {
-            //  Last byte must be 0.
-            let bs: &'static [u8] = self.bytestr;
-            assert_eq!(bs.last(), Some(&0u8));
-            bs.as_ptr() as *const ::cty::c_char
-        }
-
-        /// Return the byte string.
-        /// Fail if the last byte is not zero.
-        pub fn as_bytestr(self) -> &'static [u8] {
-            //  Last byte must be 0.
-            let bs: &'static [u8] = self.bytestr;
-            assert_eq!(bs.last(), Some(&0u8));
-            &bs
-        }
-
-        /// Fail if the last byte is not zero.
-        pub fn validate(self) {
-            //  Last byte must be 0.
-            let bs = &self.bytestr;
-            assert_eq!(bs.last(), Some(&0u8));
-        }
-
-        /// Fail if the last byte is not zero.
-        pub fn validate_bytestr(bs: &'static [u8]) {
-            //  Last byte must be 0.
-            assert_eq!(bs.last(), Some(&0u8));
-        }
-    }
-
-    static _test_static: Strn = init_strn!("hello");
-
     fn test_safe_wrap() -> MynewtResult<()> {
-        let _test_local = init_strn!("hello");
-
         "-------------------------------------------------------------";
-        #[mynewt_macros::safe_wrap(attr)] ////
+        #[macros::safe_wrap(attr)] ////
         extern "C" {
             #[doc = " Pull a single item off the event queue and call it's event"]
             #[doc = " callback."]
@@ -261,13 +29,13 @@ mod tests {
         }
         "-------------------------------------------------------------";
         /*
-        #[mynewt_macros::safe_wrap(attr)] ////
-        extern "C" {
-            #[doc = " Retrieves the default event queue processed by OS main task."]
-            #[doc = ""]
-            #[doc = " Return:                      The default event queue."]
-            pub fn os_eventq_dflt_get() -> *mut os_eventq;
-        }
+            #[mynewt_macros::safe_wrap(attr)] ////
+            extern "C" {
+                #[doc = " Retrieves the default event queue processed by OS main task."]
+                #[doc = ""]
+                #[doc = " Return:                      The default event queue."]
+                pub fn os_eventq_dflt_get() -> *mut os_eventq;
+            }
         */
         "-------------------------------------------------------------";
         /*
@@ -363,12 +131,66 @@ mod tests {
             fn dummy() {}
         */
         "-------------------------------------------------------------";
+        let _test_local = init_strn!("hello");
         Ok(())
     }
+
+    /// Represents a null-terminated byte string, suitable for passing to Mynewt APIs as `* const char`
+    pub struct Strn {
+        /// Byte string terminated with null
+        pub bytestr: &'static [u8]
     }
 
-/// Test run!() macro
-#[cfg(test_run_macro)]
+    impl Strn {
+        /// Create a new byte string. Fail if the last byte is not zero.
+        /// ```
+        /// Strn::new(b"network\0")
+        /// strn!("network")
+        /// ```
+        pub fn new(bs: &'static [u8]) -> Strn {
+            //  Last byte must be 0.
+            assert_eq!(bs.last(), Some(&0u8));
+            let res = Strn { bytestr: bs };
+            res
+        }
+
+        /// Return the byte string as a null-terminated `* const char` C-style string.
+        /// Fail if the last byte is not zero.
+        pub fn as_cstr(self) -> *const ::cty::c_char {
+            //  Last byte must be 0.
+            let bs: &'static [u8] = self.bytestr;
+            assert_eq!(bs.last(), Some(&0u8));
+            bs.as_ptr() as *const ::cty::c_char
+        }
+
+        /// Return the byte string.
+        /// Fail if the last byte is not zero.
+        pub fn as_bytestr(self) -> &'static [u8] {
+            //  Last byte must be 0.
+            let bs: &'static [u8] = self.bytestr;
+            assert_eq!(bs.last(), Some(&0u8));
+            &bs
+        }
+
+        /// Fail if the last byte is not zero.
+        pub fn validate(self) {
+            //  Last byte must be 0.
+            let bs = &self.bytestr;
+            assert_eq!(bs.last(), Some(&0u8));
+        }
+
+        /// Fail if the last byte is not zero.
+        pub fn validate_bytestr(bs: &'static [u8]) {
+            //  Last byte must be 0.
+            assert_eq!(bs.last(), Some(&0u8));
+        }
+    }
+
+    static _test_static: Strn = init_strn!("hello");
+}
+
+/// Test CBOR encoding
+#[cfg(feature = "test_cbor")]
 mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     // use super::*;
@@ -406,5 +228,192 @@ mod tests {
         //assert_eq!(1, 1);
         test_run();
         test_safe_wrap();
+    }
+}
+
+/// Test macro hygiene
+#[cfg(feature = "test_macro_hygiene")]
+mod test_macro_hygiene {
+    /// Given an expression `e`, add 88. If identifier `i` and statement block `blk` are specified, add `i` to the result and execute `blk`.
+    macro_rules! add_88 {
+        // If `e` is an expression like `123`, `i * 2`, `func(i)`...
+        ($e:expr) => {
+            {
+                // Add 88 to the expression and return it.
+                $e + 88
+            }
+        };
+        // If `e` is an expression, `i` is an identifier like `myvar`, `blk` is a block of statements...
+        ($e:expr, $i:ident, $blk:block) => {
+            {
+                // Add 88 and the value of `i` to the expression.
+                let result = $e + $i + 88;
+                // Execute the code block.
+                $blk;
+                // Return the result.
+                result
+            }
+        }
+    }
+
+    ///  Macro to dump all tokens received as a literal string, e.g.
+    ///  `d!(a b c)` returns `"a b c"`
+    macro_rules! d {
+    //  This rule matches zero or more tokens.
+    ($($token:tt)*) => {
+        //  For all matched tokens, convert into a string.
+        stringify!($($token)*)
+    };
+    }
+
+    macro_rules! parse {
+    // Helper macro to parse a JSON `key: value` entry. The entry should be followed by a trailing comma.
+    // For example: When parsing the JSON code `{ "device": "010203" , (omitted) }`, the macro will be called like this:
+    // ```
+    // parse!( @json @object context ["device"] ("010203") , (omitted) )
+    // ```
+    (@$enc:ident @object $obj:ident [$($key:tt)+] ($value:expr) , $($rest:tt)*) => {
+        d!(adding key: $($key)+ value: $value to object: $obj)
+
+        //  Append to the "values" array e.g.
+        //    {"key":"device", "value":"010203"},
+        //  $crate::coap_item_str!(@$enc $obj, $($key)+, $value);
+
+        //  Continue expanding the rest of the JSON.
+        //  $crate::parse!(@$enc @object $obj () ($($rest)*) ($($rest)*));
+    };
+    }
+
+    /// Let's make some soup based on a recipe...
+    macro_rules! bad_soup {
+        // The caller shall pass in a recipe for the soup, 
+        // say `salt + 88`
+        ($recipe:expr) => {
+            {
+                // We use our own salt, say Table Salt.
+                let salt = 1;
+                // We try to use our salt, but the recipe 
+                // actually requires a different salt, 
+                // like Sea Salt.
+                $recipe
+                // Rust Compiler fails with a Hygiene Error.
+            }
+        }
+    }
+
+    /// Let's make soup the right way...
+    macro_rules! good_soup {
+        // The caller shall pass in 2 things: 
+        // Which salt to use
+        // and the recipe (like `salt + 88`)
+        ($salt:ident, $recipe:expr) => {
+            {
+                // We use whatever salt the caller passes in. 
+                // Hence we use `$salt` instead of `salt`.
+                let $salt = 1;
+                // Then we use the same salt inside the recipe.
+                $recipe
+                // It works!
+            }
+        }
+    }
+
+    /// Make an Unhygienic soup... 
+    fn make_bad_soup() -> Result<i32, ()> {
+        let soup = bad_soup!(
+            // We try to make soup with our salt and recipe...
+            88 + salt  // If you're testing other macros, comment out the `+ salt` part or the code will never compile
+            // But the salt isn't the same inside the recipe.  
+            // Rust Compiler fails with Hygiene Error:
+            // `salt` not found in this scope.
+        );
+        Ok(soup)  //  Return the cooked soup.
+    }
+
+    /// Make a Hygienic soup
+    fn make_good_soup() -> Result<i32, ()> {
+        let soup = good_soup!(
+            // First we tell the macro which salt we're using...
+            salt, 
+            // Then we tell the macro what to do with that salt.
+            88 + salt
+            //  It works!
+        );
+        Ok(soup)  //  Return the cooked soup.
+    }
+
+    /// Let's test cooking!
+    #[cfg(test)]
+    mod tests {
+        use super::*;  //  Import everything from outer scope.
+
+        /// Test the simple add_88 macro with 1 parameter.
+        #[test]
+        fn test_add_88() { 
+            assert_eq!(
+                89,
+                add_88!(1),
+            );
+        }
+
+        /// Test the extended add_88 macro with 3 parameters.
+        #[test]
+        fn test_add_88_extended() { 
+            let x = 2;
+            let y = 3;
+            assert_eq!(
+                99,
+                add_88!(
+                    //  Expression
+                    x * 4,
+                    //  Identifier
+                    y,
+                    //  Code Block
+                    { println!("The values of x and y are {} and {}", x, y); }
+                )
+            );
+            // Shows `The value of x and y are 2 and 3`. Result is 99.
+        }
+
+        #[test]
+        fn test_parse() { 
+            parse! (
+                @json @object context ["device"] ("010203") , omitted 
+            );
+        }
+
+        #[test]
+        fn test_bad_soup() { 
+            assert_eq!(
+                make_bad_soup(),
+                Ok(89)
+            );
+        }
+
+        #[test]
+        fn test_good_soup() { 
+            assert_eq!(
+                make_good_soup(),
+                Ok(89)
+            );
+        }
+    }
+
+    /// Let's start cooking!
+    fn main() {
+        trace_macros!(true);   //  Start tracing macros
+        let res = add_88!(1);
+        trace_macros!(false);  //  Stop tracing macros
+        println!("test_add_88: {}", res);
+
+        parse! (
+            @json @object context ["device"] ("010203") , (omitted) 
+        );
+        println!("bad soup: {}",
+            make_bad_soup().unwrap()
+        );
+        println!("good soup: {}",
+            make_good_soup().unwrap()
+        );
     }
 }
